82e8a9cf28bb6390ed3956f96b8f8870ee39c5a5
shakedHUJI <142309542+shakedHUJI@users.noreply.github.com>
2024-09-16 11:51:12 +0300

    modified package.json

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Initialize OpenAI
 const configuration = new Configuration({
